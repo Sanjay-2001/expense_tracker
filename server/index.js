6b0245ad38e1b9366371baed9df943e77dcc96e7
SanjayDutta <sanjaydutta976.com>
2024-12-15 10:55:04 +0530

    creating schema

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import mergedTypeDefs from "./typeDefs/index.js";
+import mergedResolvers from "./resolvers/index.js";
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+});
 
 const { url } = await startStandaloneServer(server);
 
