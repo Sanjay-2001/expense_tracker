fd17a10d9aeee239e4e59aa848562e8f3a7d2c08
SanjayDutta <sanjaydutta976.com>
2024-12-16 16:08:59 +0530

    authentication implemented

--- server/resolvers/user.resolver.js
+++ server/resolvers/user.resolver.js
@@ -6,6 +6,7 @@ const userResolver = {
     signUp: async (_, { input }, context) => {
       try {
         const { username, name, password, gender } = input;
+
         if (!username || !name || !password || !gender) {
           throw new Error("All fields are required");
         }
@@ -40,6 +41,7 @@ const userResolver = {
     login: async (_, { input }, context) => {
       try {
         const { username, password } = input;
+        if (!username || !password) throw new Error("All fields are required");
         const { user } = await context.authenticate("graphql-local", {
           username,
           password,
@@ -51,13 +53,13 @@ const userResolver = {
         throw new Error(err.message || "Internal server error");
       }
     },
-    logout: async (_, _, context) => {
+    logout: async (_, __, context) => {
       try {
         await context.logout();
-        req.session.destroy((err) => {
+        context.req.session.destroy((err) => {
           if (err) throw err;
         });
-        res.clearCookie("connect.sid");
+        context.res.clearCookie("connect.sid");
         return { message: "Logged out successfully" };
       } catch (err) {
         console.error("Error in logout: ", err);
@@ -66,7 +68,7 @@ const userResolver = {
     },
   },
   Query: {
-    authUser: async (_, _, context) => {
+    authUser: async (_, __, context) => {
       try {
         const user = await context.getUser();
         return user;
