719d190050a7f22b65ad0d90af602a09a8fd323e
SanjayDutta <sanjaydutta976.com>
2024-12-16 11:55:09 +0530

    graphql client setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,14 @@ import {
   TransactionPage,
 } from "./pages";
 import Header from "./components/ui/Header";
+import { useQuery } from "@apollo/client";
+import { GET_AUTHENTICATED_USER } from "./graphql/mutations/transaction.mutation";
 
 function App() {
-  const authUser = true;
+  const { loading, data, error } = useQuery(GET_AUTHENTICATED_USER);
   return (
     <>
-      {authUser && <Header />}
+      {data?.authUser && <Header />}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
